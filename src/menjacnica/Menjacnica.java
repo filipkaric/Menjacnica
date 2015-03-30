59169b0c80705a8caf8f4f3db7893cb04f794ba4
filipkaric <kfilip94@gmail.com>
2015-03-30 19:07:15 +0200

    Dodata razlicita metoda dodajKurs na master grani

--- src/menjacnica/Menjacnica.java
+++ src/menjacnica/Menjacnica.java
@@ -1,6 +1,7 @@
 package menjacnica;
 
 import java.util.GregorianCalendar;
+import java.util.LinkedList;
 
 import kurs.Kurs;
 import menjacnicainterfejs.MenjacnicaInterfejs;
@@ -9,8 +10,13 @@ public class Menjacnica implements MenjacnicaInterfejs{
 
 	public void dodajKurs(GregorianCalendar datum, double prodajniKurs,
 			double srednjiKurs, double kupovniKurs) {
-		// TODO Auto-generated method stub
-		
+		Kurs k = new Kurs();
+		LinkedList<Kurs> kurs = new LinkedList<Kurs>();
+		k.setDatum(datum);
+		k.setKupovniKurs(kupovniKurs);
+		k.setSrednjiKurs(srednjiKurs);
+		k.setProdajniKurs(prodajniKurs);
+		kurs.add(k);
 	}
 
 	public Kurs obrisiKurs(GregorianCalendar datum) {
